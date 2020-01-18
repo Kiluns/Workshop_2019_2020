1d02a08bb7ea3df68330d13f86cc3eaf089b3810
Impair <andriesmartin@gmail.com>
2020-01-18 15:05:09 +0100

    protoTuto + controller

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -46,8 +46,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            float h = CrossPlatformInputManager.GetAxis("Vertical");
+            float v = CrossPlatformInputManager.GetAxis("Horizontal");
             bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
