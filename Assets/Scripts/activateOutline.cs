8a7382b24dea5ca5cc6c8b0ff2290c4b4f9bf2fa
Impair <andriesmartin@gmail.com>
2020-02-16 21:11:29 +0100

    nivo tuto 1ertest d'échelle(avec les dimensions du schéma) + mécanique d'outline à l'approche du joueur

---
+++ Assets/Scripts/activateOutline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateOutline : MonoBehaviour
+{
+public GameObject targetToOutline;
+    
+    void OnTriggerEnter(Collider Player)
+    {
+        if (Player.attachedRigidbody)
+        {
+            Debug.Log("outline activé");
+            targetToOutline.GetComponent<Outline>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider Player)
+    {
+        if (Player.attachedRigidbody)
+        {
+            Debug.Log("outline désactivé");
+            targetToOutline.GetComponent<Outline>().enabled = false;
+        }
+    }
+}
