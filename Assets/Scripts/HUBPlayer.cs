032e3f0f2b47debf60c66c34400b7f7a9b8832ac
Impair <andriesmartin@gmail.com>
2020-04-29 19:00:10 +0200

    Ajout de l'animation de marche brouillon

--- Assets/Scripts/HUBPlayer.cs
+++ Assets/Scripts/HUBPlayer.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class HUBPlayer : MonoBehaviour
 {
+    public Animator anim;
     public float Speed = 1.5f;
     private bool isFlipped;
 
     void Start()
     {
         isFlipped = false;
+    }
+
+    void Update()
+    {
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
     }
 
     void FixedUpdate()
@@ -19,7 +25,7 @@ public class HUBPlayer : MonoBehaviour
             if (!isFlipped)
             {
                 isFlipped = true;
-                gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().flipX = false;
             }
             transform.Translate(Speed * Time.deltaTime, 0f, 0f);
         }
@@ -28,7 +34,7 @@ public class HUBPlayer : MonoBehaviour
             if (isFlipped)
             {
                 isFlipped = false;
-                gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().flipX = true;
             }
             transform.Translate(-Speed * Time.deltaTime, 0f, 0f);
         }
