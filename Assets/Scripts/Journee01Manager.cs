62d66bdbcf147f1215d0af01fbe7f7e59a26ce1d
Kiluns <nicolas.kubacki@outlook.com>
2020-05-01 13:57:17 +0200

    Ajout du GameObject LevelManager, du script journee01Manager et code de l'apparition de la clé dans l'UI

---
+++ Assets/Scripts/Journee01Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Journee01Manager : MonoBehaviour
+{
+    public bool possedeCle = false;
+
+    public GameObject cleUI;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Si clé en possession : apparition de l'image dans l'UI, ouverture de la porte de sortie de l'énigme (à faire).
+        if (possedeCle == true)
+        {
+            cleUI.SetActive(true);
+        }
+        //Si clé retirée : disparition de l'image dans l'UI.
+        else
+        {
+            cleUI.SetActive(false);
+        }
+    }
+}
