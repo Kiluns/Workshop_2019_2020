a3963c27835a9cd6b04e304444e5b92617459303
Kiluns <nicolas.kubacki@outlook.com>
2020-05-05 22:05:29 +0200

    Trie des objets de la premiere journee + début armoire de la clee

---
+++ Assets/Scripts/Journee01/ArmoireCle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmoireCle : MonoBehaviour
+{
+    public GameObject Level01Manager;
+    public Animator AnimPorte;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("detected");
+            
+        }
+    }
+}
