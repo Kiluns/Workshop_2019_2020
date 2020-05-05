09374298e4f925055b9839e2ae66dba7869e540c
Kiluns <nicolas.kubacki@outlook.com>
2020-05-06 00:06:28 +0200

    Ouverture de l'amoire à clé et récupération de la clé

---
+++ Assets/Scripts/Journee01/ArmoireClee/ArmoireCle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmoireCle : MonoBehaviour
+{
+    public Animator AnimPorte;
+
+    private bool peutOuvrir = false;
+    private bool cleeRecuperer = false;
+
+    private void Update()
+    {
+        Interaction();
+    }
+
+    //On autorise le joueur à interagir dans le trigger.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            peutOuvrir = true;
+        }
+    }
+
+    //On refuse au joueur l'intéraction hors du trigger.
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            peutOuvrir = false;
+        }
+    }
+
+    //Appelée dans l'update, permet l'interaction, conditions vérifiée seulement dans le trigger.
+    //L'obtention de la clée est dans le script PorteArmoireCle et est un AnimEvent.
+    private void Interaction()
+    {
+        if(peutOuvrir == true && cleeRecuperer == false && Input.GetKeyDown("e"))
+        {
+            AnimPorte.SetTrigger("Interagis");
+            cleeRecuperer = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Journee01/ArmoireClee/PorteArmoireCle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorteArmoireCle : MonoBehaviour
+{
+    public GameObject Level01Manager;
+
+    //Obtention de la clee, appelée en animEvent.
+    public void ObtiensClee()
+    {
+        Level01Manager.GetComponent<Journee01Manager>().possedeCle = true;
+    }
+}
