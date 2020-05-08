ee1e084c14ba6def7f80b33b2c6e6944168db8d8
Impair <andriesmartin@gmail.com>
2020-05-09 01:30:19 +0200

    Ajout des WIP plus avancement sur l'animation de marche

---
+++ Assets/Image/Casting/WIPNextScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WIPNextScene : MonoBehaviour
+{
+    int mySceneIndex;
+    public void NextScene()
+    {
+        mySceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(mySceneIndex);
+    }
+}
