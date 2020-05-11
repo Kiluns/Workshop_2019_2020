13e6ba85026d2c965dde96cf3ca56dc9d4e56143
Kiluns <nicolas.kubacki@outlook.com>
2020-05-11 16:01:01 +0200

    Affichage du script de la journée et retire le script de l'écran avec un trigger et une anim.

---
+++ Assets/Scripts/RetireScriptEcran.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetireScriptEcran : MonoBehaviour
+{
+    public GameObject script;
+    private Animator scriptAnim;
+
+    private void Start()
+    {
+        scriptAnim = script.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scriptAnim.SetTrigger("Retire");
+    }
+}
