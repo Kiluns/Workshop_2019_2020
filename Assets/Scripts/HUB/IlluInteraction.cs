bb55c7ce6cd8f1852acefc8de0834ea665c8e870
Impair <andriesmartin@gmail.com>
2020-05-11 11:58:42 +0200

    Ajout HUB Illustrations + Canvas + Début du script qui gère les illus

---
+++ Assets/Scripts/HUB/IlluInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IlluInteraction : MonoBehaviour
+{
+    // myIllu représente le numéro de l'illu à afficher : 
+    //       1 - Frigo
+    //       2 - Téléphone
+    //       3 - Télé
+    //       4 - Canapé
+    //       5 - Cadres
+    //       6 - Miroir
+    //       7 - Fenêtre
+
+    [HideInInspector]
+    public int myIllu = 0; 
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void BlackCrossFade()
+    {
+
+    }
+
+    private void UnIllu()
+    {
+
+    }
+}
