7fde90f498799bf773695d947967662c61a9a93f
Kiluns <nicolas.kubacki@outlook.com>
2020-05-15 10:18:56 +0200

    Début de la poursuite et la fin du level changement de la caméra et de la vitesse du joueur et réinitialisation de ces dernières

--- Assets/Scripts/Journee01/Journee01Manager.cs
+++ Assets/Scripts/Journee01/Journee01Manager.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Colorful;
+using UnityStandardAssets;
 
 public class Journee01Manager : MonoBehaviour
 {
     public bool possedeCle = false;
     public bool placementArmoiresdecide = false;
+    public bool startMannequin = false;
+    public bool cameraModifiee = false;
+    public float vitessePoursuite = 1f;
+    private float controllerRunSpeed;
+    private float controllerWalkSpeed;
 
     public GameObject cleUI;
     public GameObject porteFinPremierCouloir;
     public GameObject porteFermeeCle;
+    public GameObject playerCamera;
+    public GameObject playerController;
+    public GameObject porteFin;
+    public GameObject mannequin;
+
+    private void Start()
+    {
+        controllerWalkSpeed = playerController.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_WalkSpeed;
+        controllerRunSpeed = playerController.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_RunSpeed;
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,5 +43,36 @@ public class Journee01Manager : MonoBehaviour
         {
             cleUI.SetActive(false);
         }
+
+        //Change la caméra et la vitesse du joueur
+        if(startMannequin == true && cameraModifiee == false)
+        {
+            playerCamera.GetComponent<Wiggle>().enabled = true;
+            playerCamera.GetComponent<DoubleVision>().enabled = true;
+            playerCamera.GetComponent<BilateralGaussianBlur>().enabled = true;
+            playerCamera.GetComponent<PhotoFilter>().enabled = true;
+            playerCamera.GetComponent<DirectionalBlur>().enabled = true;
+            playerController.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_WalkSpeed = vitessePoursuite;
+            playerController.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_RunSpeed = vitessePoursuite;
+            cameraModifiee = true;
+        }
+
+        //Retourne la caméra à son êtat original et la vitesse du joueur
+        if(porteFin.GetComponent<Porte>().aEssayerOuvrir == true)
+        {
+            playerCamera.GetComponent<Wiggle>().enabled = false;
+            playerCamera.GetComponent<DoubleVision>().enabled = false;
+            playerCamera.GetComponent<BilateralGaussianBlur>().enabled = false;
+            playerCamera.GetComponent<PhotoFilter>().enabled = false;
+            playerCamera.GetComponent<DirectionalBlur>().enabled = false;
+            playerController.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_WalkSpeed = controllerWalkSpeed;
+            playerController.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_RunSpeed = controllerRunSpeed;
+            porteFin.GetComponent<Porte>().fermeeACle = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        startMannequin = true;
     }
 }
