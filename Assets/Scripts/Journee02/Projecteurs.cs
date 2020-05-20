612a779bff2cb009dd80e645fec2f39e8286a878
Kiluns <nicolas.kubacki@outlook.com>
2020-05-20 15:52:30 +0200

    Projecteurs avec activation et désactivation du mur entiérement fait dans ma scène, prefab pas encore créer (anims et activation ou non de la lumière et sons encore à faire)

--- Assets/Scripts/Journee02/Projecteurs.cs
+++ Assets/Scripts/Journee02/Projecteurs.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Projecteurs : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mur;
+
+    private bool dansTrigger = false;
+    public bool murActive = true;
+
+    private void Start()
     {
-        
+        EtatDuMur();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Interaction();
+    }
+
+    private void Interaction()
+    {
+        if(Input.GetKeyDown("e") && dansTrigger == true && murActive == true)
+        {
+            mur.SetActive(false);
+            murActive = false;
+        }
+        else if(Input.GetKeyDown("e") && dansTrigger == true && murActive == false)
+        {
+            mur.SetActive(true);
+            murActive = true;
+        }
+    }
+
+    //On check et applique l'état correcte au mur.
+    private void EtatDuMur()
+    {
+        if(murActive == true)
+        {
+            mur.SetActive(true);
+        }
+        else if(murActive == false)
+        {
+            mur.SetActive(false);
+        }
+    }
+
+    //On autorise le joueur à interagir dans le trigger.
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dansTrigger = true;
+            transform.GetChild(1).gameObject.transform.GetComponentInChildren<BlinkFeedback>().isActive = true;
+        }
+    }
+
+    //On refuse au joueur l'intéraction hors du trigger.
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dansTrigger = false;
+            transform.GetChild(1).gameObject.transform.GetComponentInChildren<BlinkFeedback>().isActive = false;
+        }
     }
 }
