f22c4da299fc6cb0ad5f7b9564867142bc061e1e
Appirial <ludivine.chatin@gmail.com>
2020-05-25 10:52:47 +0200

    le tableau du real

---
+++ Assets/Scripts/Journee02/Tableau_Real.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tableau_Real : MonoBehaviour
+{
+    public GameObject tableux;
+    void OnTriggerEnter(Collider player)
+    {
+        tableux.SetActive(true);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
