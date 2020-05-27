9bc6b9f9a8294c83a6a6254c04c12094a61c4409
Appirial <ludivine.chatin@gmail.com>
2020-05-27 10:30:53 +0200

    la foudre

--- Assets/Scripts/Journee02/Tableau_Real.cs
+++ Assets/Scripts/Journee02/Tableau_Real.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Tableau_Real : MonoBehaviour
 {
+    public AudioSource foudreSX;
+
     public GameObject tableux;
     void OnTriggerEnter(Collider player)
     {
         tableux.SetActive(true);
+        foudreSX.Play(0);
     }
     
     // Update is called once per frame
