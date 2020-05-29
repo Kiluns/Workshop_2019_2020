8535b7dc0cf4e031425ed041dc22707ec30f102d
Appirial <ludivine.chatin@gmail.com>
2020-05-29 12:21:11 +0200

    Menu pause

---
+++ Assets/Scripts/Menu/MenuPause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject fps;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (GameIsPaused)
+            {
+                
+                Reprendre();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Reprendre()
+    {
+        pauseMenuUI.SetActive(false);
+        fps.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        fps.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void BTMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void BTReprendre()
+    {
+        Reprendre();
+    }
+    public void BTQuitter()
+    {
+        Application.Quit();
+    }
+}
+
