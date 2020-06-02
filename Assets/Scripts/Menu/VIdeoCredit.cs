f233f3d41b4924d5dc29dc6987c08352fa1d44f2
Appirial <ludivine.chatin@gmail.com>
2020-06-02 14:04:15 +0200

    les crédits dans le menu

---
+++ Assets/Scripts/Menu/VIdeoCredit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VIdeoCredit : MonoBehaviour
+{
+    public GameObject video;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        { 
+            video.SetActive(false); 
+        }
+    }
+    
+
+}
